bd38e65beae203a77ccf3ca9f4858df8171361a5
surajsinha2512 <61259615+surajsinha2512@users.noreply.github.com>
2020-11-19 02:36:40 +0530

    added Clock

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,48 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+          time:new Date()
+        }
+        this.interval=null;
+    }
     render() {
 
         return(
-            <>
-               
-            </>
+            <div className="Clock">
+               <h3 id="time">{this.getTimeString()}</h3>
+            </div>
         )
     }
+    componentDidMount(){
+        this.interval=setInterval(()=>{
+            this.setState({
+                time:new Date()
+            })
+        },1*1000)
+    }
+    componentWillUnmount(){
+        clearInterval(this.interval)
+    }
+    getTimeString(){
+        const currTime=this.state.time;
+        const [hours,minutes,seconds]=[currTime.getHours(),currTime.getMinutes(),currTime.getSeconds()]
+       const amOrPm =hours>=12?"PM":"AM"
+       const twelveHourFormat=hours>12?hours-12:hours;
+       const hourString=this.paddingTwoDigit(twelveHourFormat);
+     //  console.log(hourString)
+       const MinuteString=this.paddingTwoDigit(minutes)
+     //  console.log(MinuteString)
+       const secondString=this.paddingTwoDigit(seconds)
+     //  console.log(secondString)
+       return hourString+":"+MinuteString+":"+secondString+" " +amOrPm
+    }
+ 
+    paddingTwoDigit(num){
+        return num<10?("0"+num):(""+num)
+       } 
 }
 
 
